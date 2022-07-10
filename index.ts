3655a69e89db0711e49eeb3a29b4b2efbda2f2c6
Chandranshu <chandranshu040@gmail.com>
2022-07-10 14:25:40 +0530

    refactor

--- index.ts
+++ index.ts
@@ -1,21 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import 'dotenv/config';
-import { DB_CONFIG } from './src/config/db';
-const mongoose = require('mongoose');
-const Todos = require('./src/schema/todos');
 
 const app: Application = express();
 
-const { dbUrl, connectionOptions } = DB_CONFIG as any;
-
-mongoose.connect(dbUrl, connectionOptions)
-.then(() => console.log('MongoDB connected successfully'))
-.catch((err: any) => console.log("DB connection errored: ", err));
-
 app.get('/', (req: Request, res: Response) => {
-    res.json({
-        "hello": "world"
-    })
+	res.json({
+		hello: 'world'
+	});
 });
 
 const PORT: number = Number(process.env.PORT) || 8080;
